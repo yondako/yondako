ee1d3aa86aecc2699d175bb8951ff2bcb2d86546
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-03-07 18:58:33 +0900

    🎉 Initial Commits
    HonoX + Tailwind

---
+++ app/global.d.ts
@@ -0,0 +1,19 @@
+import "vite/client";
+import {} from "hono";
+
+type Head = {
+  title?: string;
+};
+
+declare module "hono" {
+  interface Env {
+    Variables: {};
+    Bindings: {};
+  }
+  interface ContextRenderer {
+    (
+      content: string | Promise<string>,
+      head?: Head,
+    ): Response | Promise<Response>;
+  }
+}
---
+++ app/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createRoute } from "honox/factory";
+import Counter from "../islands/counter";
+
+export default createRoute((c) => {
+  const name = c.req.query("name") ?? "Hono";
+  return c.render(
+    <div class="text-base text-red-800">
+      <h1>Hello, {name}!</h1>
+      <Counter />
+    </div>,
+    { title: name },
+  );
+});
