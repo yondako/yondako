56607ca873ac27850c27d63374197f363b28acac
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-03-20 15:09:50 +0900

    ✨ 404ページを追加

--- app/routes/_404.tsx
+++ app/routes/_404.tsx
@@ -1,30 +1,31 @@
-import { createRoute } from "honox/factory";
-import logoUrl from "../assets/images/logo_portrait.svg";
+import { NotFoundHandler } from "hono";
 import Button from "../components/common/Button";
 import Footer from "../components/common/Footer";
 import LandingPageLayout from "../components/layout/LandingPage";
 import { site } from "../libs/constants";
 
-export default createRoute((c) => {
+const handler: NotFoundHandler = (c) => {
   return c.render(
     <LandingPageLayout>
-      <div className="max-w-[26rem]">
-        <img className="m-auto" width={256} src={logoUrl} alt={site.name} />
-        <h1 className="mt-10 text-3xl md:text-4xl tracking-wide text-center">
-          {site.description.short}
+      <div className="max-w-[26rem] text-center">
+        <h1 className="font-bold text-4xl md:text-5xl tracking-wide">
+          NotFound
         </h1>
-        <p className="mt-6">{site.description.long}</p>
+        <p className="mt-10">ページが見つかりませんでした 🫥</p>
+        <p className="mt-1">URLが間違っていないかご確認ください</p>
         <Button asChild>
           <a className="block mx-auto mt-10 text-base" href="/">
             <span className="font-noto-emoji">🐙</span>
-            <span className="ml-2">新規登録 or ログイン</span>
+            <span className="ml-2">トップページに戻る</span>
           </a>
         </Button>
       </div>
       <Footer className="fixed inset-x-0 bottom-8" />
     </LandingPageLayout>,
     {
-      title: `${site.name} | ${site.description.short}`,
+      title: `NotFound | ${site.name}`,
     },
   );
-});
+};
+
+export default handler;
