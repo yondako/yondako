3b85ae45374dd3b3346a54ffd243b4a092cf6a80
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-03-24 21:27:27 +0900

    💥 /home -> /library に

--- app/routes/library/index.tsx
+++ app/routes/library/index.tsx
@@ -1,6 +1,6 @@
+import CommonLayout from "@/components/layout/Common";
+import { site } from "@/libs/constants";
 import { createRoute } from "honox/factory";
-import CommonLayout from "../../components/layout/Common";
-import { site } from "../../libs/constants";
 
 export default createRoute((c) => {
   return c.render(
@@ -8,7 +8,7 @@ export default createRoute((c) => {
       <h1>Hello!</h1>
     </CommonLayout>,
     {
-      title: `ホーム | ${site.name}`,
+      title: `ライブラリ | ${site.name}`,
     },
   );
 });
