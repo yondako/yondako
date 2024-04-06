7a01a20c8da6642ba80ff38e4fc920983dcefdb3
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-04-06 18:58:45 +0900

    👍 レスポンシブのブレークボイントをmd -> lgに

--- app/components/landing/Layout/index.tsx
+++ app/components/landing/Layout/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function LandingLayout({ children }: Props) {
   return (
-    <body className="flex justify-center items-center h-full p-8 bg-gradation bg-left-top bg-no-repeat bg-[length:80%] md:bg-[length:50%]">
+    <body className="flex justify-center items-center h-full p-8 bg-gradation bg-left-top bg-no-repeat bg-[length:80%] lg:bg-[length:50%]">
       {children}
       <Footer className="fixed inset-x-0 bottom-8 text-center" />
     </body>
