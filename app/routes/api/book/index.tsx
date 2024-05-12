cbe1d976feb73622dd5132237aa913f23d8fe579
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-05-12 18:48:28 +0900

    🚧 書籍情報をDBから読み書きするように

--- app/routes/api/book/index.tsx
+++ app/routes/api/book/index.tsx
@@ -1,5 +1,7 @@
 import { searchBookFromNDL } from "@/libs/ndl/api";
 import { vValidator } from "@hono/valibot-validator";
+import { PrismaD1 } from "@prisma/adapter-d1";
+import { PrismaClient } from "@prisma/client";
 import { Hono } from "hono";
 import { object, picklist } from "valibot";
 
@@ -18,26 +20,41 @@ const routes = app.post(
 
     console.log(ndlBibId, status);
 
-    // TODO:
-    // まずはDBに存在するか確認
-    // 無い場合は検索
+    // Dbに登録されているか確認
+    const adapter = new PrismaD1(c.env.DB);
+    const prismaClient = new PrismaClient({ adapter });
 
-    // NDLから書籍情報を取得
-    const results = await searchBookFromNDL({ any: ndlBibId, cnt: 1 });
+    let book = await prismaClient.book.findUnique({
+      where: {
+        ndlBibId,
+      },
+    });
 
-    if (!results || results.length <= 0) {
-      return c.json(
-        {
-          message: "書籍がみつかりませんでした",
-        },
-        404,
-      );
+    // DBに登録
+    if (!book) {
+      // NDLから書籍情報を取得
+      const results = await searchBookFromNDL({ any: ndlBibId, cnt: 1 });
+
+      if (!results || results.length <= 0) {
+        return c.json(
+          {
+            message: "書籍がみつかりませんでした",
+          },
+          404,
+        );
+      }
+
+      book = await prismaClient.book.create({
+        data: results[0],
+      });
     }
 
-    // TODO: DBに反映後のデータを返す
+    // TODO: ステータスの変更をDBに反映
+
+    // TODO: 反映後のデータを返す
     return c.json(
       {
-        info: results[0],
+        info: book,
         status,
         liked: false,
       },
