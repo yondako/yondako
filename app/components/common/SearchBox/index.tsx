f810b76247e534f6b6b8f1abc42f0fa37d215698
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-06-30 19:00:28 +0900

    👍 検索ボックスのデザインを変更

--- app/components/common/SearchBox/index.tsx
+++ app/components/common/SearchBox/index.tsx
@@ -1,18 +1,20 @@
-import { classNames } from "@/libs/classNames";
 import { ComponentPropsWithRef } from "react";
+import { twMerge } from "tailwind-merge";
 import { IconSearch } from "../Icon/Search";
 
-export default function SearchBox(props: ComponentPropsWithRef<"input">) {
+export default function SearchBox({
+  className,
+  ...props
+}: ComponentPropsWithRef<"input">) {
   return (
     <div className="relative text-text">
       <IconSearch className="absolute w-4 w-4 mt-[1px] top-1.5 left-4 text-gray-400" />
       <input
         {...props}
-        className={classNames(
+        className={twMerge(
           "w-full px-4 pl-10 py-2 text-sm border border-line rounded-full",
-          props.className,
+          className,
         )}
-        placeholder="書籍タイトル・著者名から"
       />
     </div>
   );
