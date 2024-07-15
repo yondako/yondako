c8bd10615fdcd06790ffca124cd24e98979a76ad
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-07-15 21:42:40 +0900

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.22.0
      project name = yondako
      framework = next
      framework cli = create-next-app@14.1.0
      package manager = bun@1.1.17
      wrangler = wrangler@3.63.2
      git = 2.39.3 (Apple Git-146)

--- app/constants/site.ts
+++
@@ -1,8 +0,0 @@
-export const site = {
-  name: "yondako",
-  description: {
-    short: "“よんだこと”を記録する",
-    long: "yondakoは、読んだ本を記録するだけのシンプルなWebサービスです。",
-  },
-  url: "https://yondako.com",
-};
--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  schema: "./app/db/schema/*.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: process.env.DATABASE_PATH || "",
-  },
-});
