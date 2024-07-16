228bdb49661b56a50b26483f05491374f5696877
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-07-16 23:39:23 +0900

    ✨ トップページを移植

---
+++ src/app/_components/LoginButton/index.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import IconContinueWithGithub from "@/assets/icons/continue-with-github.svg";
+import IconContinueWithGoogle from "@/assets/icons/continue-with-google.svg";
+import type { BuiltInProviderType } from "next-auth/providers";
+import { signIn } from "next-auth/react";
+import type { ReactNode } from "react";
+
+export default function LoginButton() {
+  return (
+    <div className="mt-12 flex flex-col space-y-4">
+      <Button provider="google">
+        <IconContinueWithGoogle />
+      </Button>
+      <Button provider="github">
+        <IconContinueWithGithub />
+      </Button>
+    </div>
+  );
+}
+
+type ButtonProps = {
+  provider: BuiltInProviderType;
+  children: ReactNode;
+};
+
+function Button({ provider, children }: ButtonProps) {
+  return (
+    <button
+      className="mx-auto block h-[41px] w-[189px] md:mx-0"
+      onClick={() =>
+        signIn(provider, {
+          callbackUrl: "/library/reading",
+        })
+      }
+    >
+      {children}
+    </button>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
+import imageLogo from "@/assets/images/logo_portrait.svg?url";
+import LandingLayout from "@/components/LandingLayout";
+import { site } from "@/constants/site";
+import Image from "next/image";
+import LoginButton from "./_components/LoginButton";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24" />
+    <LandingLayout>
+      <Image
+        className="mx-auto md:mx-0"
+        width={256}
+        src={imageLogo}
+        alt={site.name}
+      />
+      <h1 className="mt-12 text-center text-3xl tracking-wide md:text-left md:text-4xl">
+        {site.description.short}
+      </h1>
+      <p className="mt-6 text-center md:text-left">{site.description.long}</p>
+      <LoginButton />
+    </LandingLayout>
   );
 }
