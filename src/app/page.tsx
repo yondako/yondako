72dd7c734664bbde94ee732c9490a5a90c4b3f5e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-07-20 23:25:45 +0900

    🎨 ソート

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import imageLogo from "@/assets/images/logo_portrait.svg?url";
 import LandingLayout from "@/components/LandingLayout";
 import { site } from "@/constants/site";
+import { auth } from "@/lib/auth";
 import { generateMetadataTitle } from "@/lib/metadata";
 import Image from "next/image";
-import LoginButton from "./_components/LoginButton";
-import { auth } from "@/lib/auth";
 import { redirect } from "next/navigation";
+import LoginButton from "./_components/LoginButton";
 
 export const runtime = "edge";
 
