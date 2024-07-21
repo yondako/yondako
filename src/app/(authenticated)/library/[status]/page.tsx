2465822846b05607191439c0b517a0e1c5f000e5
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-07-21 22:42:01 +0900

    🚧 ソート順の変更

--- src/app/(authenticated)/library/[status]/page.tsx
+++ src/app/(authenticated)/library/[status]/page.tsx
@@ -4,15 +4,19 @@ import { getBooksByReadingStatus } from "@/db/queries/status.server";
 import { auth } from "@/lib/auth.server";
 import { generateMetadataTitle } from "@/lib/metadata";
 import { createSignInPath } from "@/lib/url";
-import { notFound, redirect } from "next/navigation";
-import { Suspense } from "react";
-import { is } from "valibot";
-import Layout from "../../_components/Layout";
-import Tab from "../_components/Tab";
 import {
   type ReadingStatus,
   readingStatusSchemaWithoutNone,
 } from "@/types/readingStatus";
+import { notFound, redirect } from "next/navigation";
+import Button from "@/components/Button";
+import IconSortAsc from "@/assets/icons/sort-ascending.svg";
+import IconSortDesc from "@/assets/icons/sort-descending.svg";
+import { Suspense } from "react";
+import { is, safeParse } from "valibot";
+import Layout from "../../_components/Layout";
+import Tab from "../_components/Tab";
+import { orderSchema } from "@/types/order";
 
 export const runtime = "edge";
 
@@ -22,9 +26,12 @@ type Props = {
   params: {
     status: ReadingStatus;
   };
+  searchParams: {
+    order?: "asc" | "desc";
+  };
 };
 
-export default async function Library({ params }: Props) {
+export default async function Library({ params, searchParams }: Props) {
   const session = await auth();
 
   if (!session?.user?.id) {
@@ -36,11 +43,15 @@ export default async function Library({ params }: Props) {
     notFound();
   }
 
+  // ソート順
+  const orderParseResult = safeParse(orderSchema, searchParams.order);
+  const orderType = orderParseResult.success ? orderParseResult.output : "desc";
+
   return (
     <Layout current="ライブラリ">
       <Tab current={params.status} />
       <Suspense fallback={<Loading title="読み込んでいます" />}>
-        <LibraryBookList status={params.status} />
+        <LibraryBookList status={params.status} order={orderType} />
       </Suspense>
     </Layout>
   );
@@ -48,6 +59,7 @@ export default async function Library({ params }: Props) {
 
 type LibraryBookListProps = {
   status: ReadingStatus;
+  order: "asc" | "desc";
 };
 
 async function LibraryBookList({ status }: LibraryBookListProps) {
@@ -61,10 +73,16 @@ async function LibraryBookList({ status }: LibraryBookListProps) {
 
   return (
     <div className="mt-10">
-      <h1 className="font-bold">
-        <span className="text-4xl">{books.length}</span>
-        <span className="text-base">冊</span>
-      </h1>
+      <div className="flex items-center justify-between space-x-4">
+        <h1 className="font-bold">
+          <span className="text-4xl">{books.length}</span>
+          <span className="text-base">冊</span>
+        </h1>
+        <Button className="inline-flex h-fit items-center space-x-1 px-4 py-1 text-xs">
+          <IconSortDesc className="h-5" />
+          <span>登録日が新しい順</span>
+        </Button>
+      </div>
       <BookList items={books} hideReadingStatusBadge />
     </div>
   );
