2c5b6891897e1d882a84c6ad6a50d101ad8d64b6
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-07-21 17:59:10 +0900

    👍 メタデータを追加

--- src/app/(authenticated)/search/barcode/page.tsx
+++ src/app/(authenticated)/search/barcode/page.tsx
@@ -1,5 +1,12 @@
 import { auth } from "@/lib/auth.server";
+import { generateMetadataTitle } from "@/lib/metadata";
+import { createSignInPath } from "@/lib/url";
+import { redirect } from "next/navigation";
 import Layout from "../../_components/Layout";
+
+export const runtime = "edge";
+
+export const metadata = generateMetadataTitle("バーコードで探す");
 
 export default async function SearchBarcode() {
   const session = await auth();
--- src/app/(authenticated)/settings/page.tsx
+++ src/app/(authenticated)/settings/page.tsx
@@ -1,8 +1,13 @@
 import { auth } from "@/lib/auth.server";
+import { generateMetadataTitle } from "@/lib/metadata";
+import { createSignInPath } from "@/lib/url";
+import { redirect } from "next/navigation";
 import Layout from "../_components/Layout";
 import LogoutButton from "./_components/LogoutButton";
 
 export const runtime = "edge";
+
+export const metadata = generateMetadataTitle("設定");
 
 export default async function Settings() {
   const session = await auth();
