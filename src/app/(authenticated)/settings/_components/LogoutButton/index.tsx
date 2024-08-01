8b5ec4a29a1b09ceaff53e286d818694b394b919
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-01 21:48:09 +0900

    ✨ 設定ページを実装

--- src/app/(authenticated)/settings/_components/LogoutButton/index.tsx
+++ src/app/(authenticated)/settings/_components/LogoutButton/index.tsx
@@ -1,5 +1,6 @@
 import Button from "@/components/Button";
 import { signOut } from "@/lib/auth.server";
+import IconLogout from "@/assets/icons/logout.svg";
 
 export default function LogoutButton() {
   const handleSubmit = async () => {
@@ -9,8 +10,14 @@ export default function LogoutButton() {
   };
 
   return (
-    <form action={handleSubmit}>
-      <Button type="submit">ログアウト</Button>
+    <form className="mt-4" action={handleSubmit}>
+      <Button
+        className="flex w-full items-center justify-center space-x-2 md:max-w-48"
+        type="submit"
+      >
+        <IconLogout className="h-5 w-5" />
+        <span>ログアウト</span>
+      </Button>
     </form>
   );
 }
--- src/app/(authenticated)/settings/page.tsx
+++ src/app/(authenticated)/settings/page.tsx
@@ -4,6 +4,8 @@ import { createSignInPath } from "@/lib/url";
 import { redirect } from "next/navigation";
 import Layout from "../_components/Layout";
 import LogoutButton from "./_components/LogoutButton";
+import Link from "next/link";
+import Button from "@/components/Button";
 
 export const runtime = "edge";
 
@@ -18,7 +20,22 @@ export default async function Settings() {
 
   return (
     <Layout current="設定">
+      <h2 className="font-bold text-xl">アカウント</h2>
+      <div className="mt-4 flex flex-col text-sm md:flex-row md:items-center md:space-x-8">
+        <p>ユーザーID</p>
+        <p className="mt-2 rounded-full bg-card px-4 py-2 md:mt-0">
+          {session.user.id}
+        </p>
+      </div>
       <LogoutButton />
+
+      <h2 className="mt-16 font-bold text-xl">退会</h2>
+      <Button
+        className="mt-4 block w-full border-0 bg-rose-700 text-background md:max-w-48"
+        asChild
+      >
+        <Link href="/goodbye">アカウントを削除</Link>
+      </Button>
     </Layout>
   );
 }
