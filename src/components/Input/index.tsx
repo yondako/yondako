15e5b2b1f32f5c23e48690bf24819fd8ef9e0b95
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-06 18:28:16 +0900

    🚑 refを渡せるように

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,17 +1,17 @@
-import type { ComponentProps } from "react";
+import { type ComponentProps, forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-export default function Input({
-  className,
-  ...props
-}: ComponentProps<"input">) {
-  return (
-    <input
-      {...props}
-      className={twMerge(
-        "w-full rounded-full bg-card px-4 py-2 text-base placeholder:text-text-sub focus:outline-tako md:text-sm",
-        className,
-      )}
-    />
-  );
-}
+export default forwardRef<HTMLInputElement, ComponentProps<"input">>(
+  function Input({ className, ...props }, ref) {
+    return (
+      <input
+        {...props}
+        ref={ref}
+        className={twMerge(
+          "w-full rounded-full bg-card px-4 py-2 text-base placeholder:text-text-sub focus:outline-tako md:text-sm",
+          className,
+        )}
+      />
+    );
+  },
+);
