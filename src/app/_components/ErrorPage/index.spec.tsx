52685b2a7af4846ddc403255d4f32934ad1c50ac
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-08 14:54:56 +0900

    💚 テストを追加

---
+++ src/app/_components/ErrorPage/index.spec.tsx
@@ -0,0 +1,13 @@
+import { test, expect } from "bun:test";
+import { render, screen } from "@testing-library/react";
+import ErrorPage from ".";
+
+test("タイトルがある", () => {
+  render(
+    <ErrorPage title="エラータイトル">
+      <p>ここにテキストを入れる</p>
+    </ErrorPage>,
+  );
+
+  expect(screen.getByRole("heading").textContent).toBe("エラータイトル");
+});
