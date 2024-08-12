f48d73e85c2574aa7e16acf5c986aa25aed4e0ee
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-12 11:18:49 +0900

    🚧 CSPを設定してみた

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,57 @@
 import { type NextRequest, NextResponse } from "next/server";
 
+const isDev = process.env.NODE_ENV === "development";
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+      missing: [
+        { type: "header", key: "next-router-prefetch" },
+        { type: "header", key: "purpose", value: "prefetch" },
+      ],
+    },
+  ],
+};
+
 export function middleware(request: NextRequest) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
-  const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'nonce-${nonce}';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    upgrade-insecure-requests;
-`;
+
+  const cspList = {
+    "default-src": ["'self'"],
+    "script-src": [
+      "'self'",
+      `'nonce-${nonce}'`,
+      "'strict-dynamic'",
+      isDev && "'unsafe-eval'",
+      isDev && "'unsafe-inline'",
+    ],
+    "style-src": ["'self'", isDev ? "'unsafe-inline'" : `'nonce-${nonce}'`],
+    "img-src": ["'self'", "blob:", "data:"],
+    "font-src": ["'self'"],
+    "object-src": ["https://ndlsearch.ndl.go.jp"],
+    "base-uri": ["'self'"],
+    "form-action": ["'self'"],
+    "frame-ancestors": ["'none'"],
+    "upgrade-insecure-requests": [],
+  };
+
+  const cspHeader = Object.entries(cspList)
+    .map(([key, values]) => {
+      const value = values.filter((e) => typeof e === "string").join(" ");
+      return values.length === 0 ? key : `${key} ${value}`;
+    })
+    .join(";");
 
   // Replace newline characters and spaces
-  const contentSecurityPolicyHeaderValue = cspHeader
-    .replace(/\s{2,}/g, " ")
-    .trim();
+  const contentSecurityPolicyHeaderValue = `${cspHeader};`;
 
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-nonce", nonce);
@@ -33,6 +66,7 @@ export function middleware(request: NextRequest) {
       headers: requestHeaders,
     },
   });
+
   response.headers.set(
     "Content-Security-Policy",
     contentSecurityPolicyHeaderValue,
