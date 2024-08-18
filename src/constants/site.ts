39715ab009ad88928c9a16184c12668104c5797d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-18 17:06:36 +0900

    💬 説明文を調整

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -2,7 +2,7 @@ export const site = {
   name: "yondako",
   description: {
     short: "“よんだこと”をわすれない",
-    long: `yondakoは、"よんだこと"をわすれないための読書記録サービスです。`,
+    long: `yondako は、"よんだこと"をわすれないための読書記録サービスです。`,
   },
   url: "https://yondako.com",
 };
