c33104cc514cd30d2f68d798ad611aec86bb4fdd
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-18 17:30:19 +0900

    🚑 fillOpacity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default async function Home() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#fdf3eb"
-            fill-opacity="1"
+            fillOpacity="1"
             d="M0,224L80,229.3C160,235,320,245,480,224C640,203,800,149,960,138.7C1120,128,1280,160,1360,176L1440,192L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"
           />
         </svg>
@@ -84,7 +84,7 @@ export default async function Home() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
           <path
             fill="#fdf3eb"
-            fill-opacity="1"
+            fillOpacity="1"
             d="M0,224L80,229.3C160,235,320,245,480,224C640,203,800,149,960,138.7C1120,128,1280,160,1360,176L1440,192L1440,0L1360,0C1280,0,1120,0,960,0C800,0,640,0,480,0C320,0,160,0,80,0L0,0Z"
           />
         </svg>
