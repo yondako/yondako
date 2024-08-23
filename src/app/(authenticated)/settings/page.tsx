06168693811ff8fc7c78b63cb949897003b5b211
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-23 15:20:04 +0900

    👍 設定ページのレイアウトを変更

--- src/app/(authenticated)/settings/page.tsx
+++ src/app/(authenticated)/settings/page.tsx
@@ -6,6 +6,7 @@ import { createSignInPath } from "@/lib/path";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import LogoutButton from "./_components/LogoutButton";
+import SettingProperty from "./_components/SettingProperty";
 
 export const runtime = "edge";
 
@@ -21,17 +22,36 @@ export default async function Settings() {
   return (
     <>
       <h2 className="font-bold text-xl">アカウント</h2>
-      <LogoutButton />
+      <div className="mt-4 w-full space-y-4 rounded-2xl bg-tertiary-background p-8">
+        <SettingProperty
+          title="ユーザーID"
+          description="サポート時などに使用します"
+        >
+          <p className="break-all text-primary-text text-sm">
+            {session.user.id}
+          </p>
+        </SettingProperty>
 
-      <h2 className="mt-12 font-bold text-xl">退会</h2>
-      <Button
-        className="mt-4 block w-full border-0 bg-rose-700 text-primary-background text-sm lg:max-w-48"
-        asChild
-      >
-        <Link href="/settings/goodbye">アカウントを削除</Link>
-      </Button>
+        <SettingProperty
+          title="ログアウト"
+          description="このデバイスからログアウトして、トップページに戻ります"
+        >
+          <LogoutButton />
+        </SettingProperty>
 
-      <Footer className="mt-16 text-center lg:hidden" />
+        <SettingProperty
+          title="退会"
+          description="アカウントを削除して退会します"
+        >
+          <Button
+            className="block w-full border-0 bg-rose-700 text-primary-background text-sm sm:w-48"
+            asChild
+          >
+            <Link href="/settings/goodbye">アカウントを削除</Link>
+          </Button>
+        </SettingProperty>
+      </div>
+      <Footer className="mt-8 text-center lg:hidden" />
     </>
   );
 }
