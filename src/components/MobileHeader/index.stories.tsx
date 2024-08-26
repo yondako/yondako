0d6efb341f41c510ae5e0c3575ccfb0f4f6a27a8
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-26 15:15:15 +0900

    👍 ロゴ色を外から当てられるように

---
+++ src/components/MobileHeader/index.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import MobileHeader from ".";
+
+const meta: Meta<typeof MobileHeader> = {
+  title: "Navigation/MobileHeader",
+  component: MobileHeader,
+};
+
+export default meta;
+type Story = StoryObj<typeof MobileHeader>;
+
+export const Default: Story = {};
