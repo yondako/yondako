ee77bfaa0f64b73d65c8139e9295b40ef944426c
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-28 18:21:28 +0900

    ✨ モバイルへの誘導ページ

--- src/app/(framed)/search/barcode/mobile-exclusive/page.tsx
+++ src/app/(framed)/search/barcode/mobile-exclusive/page.tsx
@@ -1,7 +1,26 @@
+import ImageQrCode from "@/assets/images/qr-search-barcode.svg";
+import ErrorMessage from "@/components/ErrorMessage";
+
 export default function MobileExclusive() {
   return (
-    <div>
-      <h2>この機能はモバイルからのみご利用可能です 🙏</h2>
+    <div className="flex h-full items-center">
+      <ErrorMessage
+        title="この機能はスマホからのみ利用可能です"
+        decoration={
+          <>
+            <span className="absolute top-0 left-0 text-4xl">📱</span>
+            <span className="-right-2 absolute bottom-7 text-3xl">💻️</span>
+          </>
+        }
+      >
+        <p className="mt-3">よかったらスマホからアクセスしてみてください 🙏</p>
+        <p className="mt-1 text-xs">
+          (需要があれば、デスクトップ対応するかもしれません)
+        </p>
+        <div className="mx-auto mt-12 w-48 rounded-2xl bg-tertiary-background p-4">
+          <ImageQrCode className="text-accent" />
+        </div>
+      </ErrorMessage>
     </div>
   );
 }
