f2a53a317a317ea3c5adbd9499c9224f8b60f3e4
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-28 21:40:38 +0900

    👍 カード全体をドロワーを開くトリガーに

--- src/components/BookCard/index.tsx
+++ src/components/BookCard/index.tsx
@@ -30,38 +30,35 @@ export default function BookCard({ data }: BookCardProps) {
 
   return (
     <div className="relative w-full text-left text-primary-foreground">
-      <div className="mt-8 flex h-40 w-full flex-col justify-between overflow-hidden rounded-2xl bg-tertiary-background p-4 pl-36">
-        <div className="space-y-1">
-          <p className="line-clamp-3 font-bold text-sm leading-5">
-            {data.detail.title}
-          </p>
-
-          {data.detail.authors && (
-            <p className="line-clamp-1 text-secondary-foreground text-xxs">
-              {data.detail.authors.join(", ")}
+      <BookDrawer data={data} {...formProps}>
+        <button className="mt-8 flex h-40 w-full flex-col justify-between overflow-hidden rounded-2xl bg-tertiary-background p-4 pl-36 text-left">
+          <div className="space-y-1">
+            <p className="line-clamp-3 font-bold text-sm leading-5">
+              {data.detail.title}
             </p>
-          )}
-        </div>
 
-        <div className="flex justify-between">
-          <BookReadingStatusForm
-            className="shrink-0"
-            compact
-            bookId={data.detail.ndlBibId}
-            bookTitle={data.detail.title}
-            {...formProps}
-          />
+            {data.detail.authors && (
+              <p className="line-clamp-1 text-secondary-foreground text-xxs">
+                {data.detail.authors.join(", ")}
+              </p>
+            )}
+          </div>
+        </button>
+      </BookDrawer>
 
-          <BookDrawer data={data} {...formProps}>
-            <button className="rounded-2xl bg-tertiary-background p-1 transition hover:brightness-95">
-              <IconDotsVertical className="h-4 w-4" />
-            </button>
-          </BookDrawer>
-        </div>
-      </div>
+      <BookReadingStatusForm
+        className="absolute bottom-4 left-36"
+        compact
+        bookId={data.detail.ndlBibId}
+        bookTitle={data.detail.title}
+        {...formProps}
+      />
+
+      {/* ドロワーを開くボタンの名残り */}
+      <IconDotsVertical className="pointer-events-none absolute right-5 bottom-6 block h-4 w-4 rounded-2xl" />
 
       <BookThumbnail
-        className="absolute top-4 left-4 w-28 border-4 border-tertiary-background shadow-xl"
+        className="pointer-events-none absolute top-4 left-4 w-28 border-4 border-tertiary-background shadow-xl"
         src={data.detail.thumbnailUrl}
       />
     </div>
