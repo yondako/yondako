8f0a640e1707e74380b4443c134f506ddb876989
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-29 17:52:03 +0900

    ♻️ 不要なPropsを受け取らないように

--- src/components/BookDetail/index.tsx
+++ src/components/BookDetail/index.tsx
@@ -1,9 +1,16 @@
+"use client";
+
 import { useMedia } from "react-use";
 import BookDetailDialog from "./Dialog";
 import BookDetailDrawer from "./Drawer";
 import type { BookDetailProps } from "./props";
 
-export default function BookDetail(props: BookDetailProps) {
+export default function BookDetail(
+  props: Omit<
+    BookDetailProps,
+    "open" | "onOpenChange" | "defaultOpen" | "modal"
+  >,
+) {
   const isDesktopWidth = useMedia("(min-width: 1024px)"); // Tailwind の lg 幅
 
   return isDesktopWidth ? (
