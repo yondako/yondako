4bb5b43eeb530ff0506f9add5831e2b04391043d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-30 15:17:54 +0900

    🚑 デプロイ環境で document is not defined が出ていた
    Lottieが原因だったので、dynamic import で解決

---
+++ src/components/Loading/AnimationTako.tsx
@@ -0,0 +1,12 @@
+import { Player } from "@lottiefiles/react-lottie-player";
+
+export default function AnimationTako() {
+  return (
+    <Player
+      className="h-24 w-24"
+      autoplay
+      loop
+      src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f419/lottie.json"
+    />
+  );
+}
