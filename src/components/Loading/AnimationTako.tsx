70d59b12b95f88f8f442b3eca977497df1499880
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-08-30 15:45:26 +0900

    👍 AnimationEmojiのlottieファイルをリポジトリに含めた

--- src/components/Loading/AnimationTako.tsx
+++ src/components/Loading/AnimationTako.tsx
@@ -1,12 +1,8 @@
+import animatedEmojiOctpus from "@/assets/animated-emoji/1f419/lottie.json";
 import { Player } from "@lottiefiles/react-lottie-player";
 
 export default function AnimationTako() {
   return (
-    <Player
-      className="h-24 w-24"
-      autoplay
-      loop
-      src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f419/lottie.json"
-    />
+    <Player className="h-24 w-24" autoplay loop src={animatedEmojiOctpus} />
   );
 }
