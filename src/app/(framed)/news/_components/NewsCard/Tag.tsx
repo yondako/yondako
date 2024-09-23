4136c77624dfe4b7140222efe692a598eed23c5f
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-09-23 21:45:40 +0900

    👍 aria-labelの追加

--- src/app/(framed)/news/_components/NewsCard/Tag.tsx
+++ src/app/(framed)/news/_components/NewsCard/Tag.tsx
@@ -4,7 +4,10 @@ type Props = {
 
 export default function Tag({ name }: Props) {
   return (
-    <span className="block w-fit rounded-full bg-accent px-3 py-0.5 text-tertiary-background text-xs">
+    <span
+      className="block w-fit rounded-full bg-accent px-3 py-0.5 text-tertiary-background text-xs"
+      aria-label={`タグ: ${name}`}
+    >
       {name}
     </span>
   );
