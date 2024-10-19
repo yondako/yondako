b4a72874f093560393da4dbad5b2dbd89e21c39e
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-10-20 02:29:22 +0900

    🔧 調整

--- .storybook/playwright.config.ts
+++ .storybook/playwright.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   snapshotPathTemplate: "../__snapshots__/{testFilePath}/{arg}{ext}",
   reporter: "html",
   timeout: 30 * 1000,
-  retries: 2,
+  retries: 1,
   fullyParallel: true,
   expect: {
     timeout: 10 * 1000,
