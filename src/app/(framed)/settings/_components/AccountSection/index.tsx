c1a0ed9f7541d6ddb62a39afbc3f7ef1c670d4ca
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-12-12 21:13:08 +0900

    ♻️ (settings): セクションをコンポーネント化

---
+++ src/app/(framed)/settings/_components/AccountSection/index.tsx
@@ -0,0 +1,43 @@
+import Button from "@/components/Button";
+import Link from "next/link";
+import LogoutButton from "./LogoutButton";
+import SettingProperty from "./SettingProperty";
+
+type Props = {
+  userId: string;
+};
+
+export default function AccountSection({ userId }: Props) {
+  return (
+    <>
+      <h2 className="font-bold text-xl">アカウント</h2>
+      <div className="mt-4 w-full space-y-6 rounded-2xl bg-tertiary-background p-8">
+        <SettingProperty
+          title="ユーザーID"
+          description="サポート時などに使用します"
+        >
+          <p className="break-all text-primary-foreground text-sm">{userId}</p>
+        </SettingProperty>
+
+        <SettingProperty
+          title="ログアウト"
+          description="このデバイスからログアウトして、トップページに戻ります"
+        >
+          <LogoutButton />
+        </SettingProperty>
+
+        <SettingProperty
+          title="退会"
+          description="アカウントを削除して退会します"
+        >
+          <Button
+            className="block w-full border-0 bg-rose-700 text-primary-background text-sm sm:w-48"
+            asChild
+          >
+            <Link href="/settings/goodbye">アカウントを削除</Link>
+          </Button>
+        </SettingProperty>
+      </div>
+    </>
+  );
+}
--- src/app/(framed)/settings/page.tsx
+++ src/app/(framed)/settings/page.tsx
@@ -1,12 +1,9 @@
-import Button from "@/components/Button";
 import Footer from "@/components/Footer";
 import { auth } from "@/lib/auth";
 import { generateMetadataTitle } from "@/lib/metadata";
 import { createSignInPath } from "@/lib/path";
-import Link from "next/link";
 import { redirect } from "next/navigation";
-import LogoutButton from "./_components/LogoutButton";
-import SettingProperty from "./_components/SettingProperty";
+import AccountSection from "./_components/AccountSection";
 
 export const runtime = "edge";
 
@@ -21,36 +18,7 @@ export default async function Settings() {
 
   return (
     <>
-      <h2 className="font-bold text-xl">アカウント</h2>
-      <div className="mt-4 w-full space-y-6 rounded-2xl bg-tertiary-background p-8">
-        <SettingProperty
-          title="ユーザーID"
-          description="サポート時などに使用します"
-        >
-          <p className="break-all text-primary-foreground text-sm">
-            {session.user.id}
-          </p>
-        </SettingProperty>
-
-        <SettingProperty
-          title="ログアウト"
-          description="このデバイスからログアウトして、トップページに戻ります"
-        >
-          <LogoutButton />
-        </SettingProperty>
-
-        <SettingProperty
-          title="退会"
-          description="アカウントを削除して退会します"
-        >
-          <Button
-            className="block w-full border-0 bg-rose-700 text-primary-background text-sm sm:w-48"
-            asChild
-          >
-            <Link href="/settings/goodbye">アカウントを削除</Link>
-          </Button>
-        </SettingProperty>
-      </div>
+      <AccountSection userId={session.user.id} />
       <Footer className="mt-8 text-center lg:hidden" />
     </>
   );
