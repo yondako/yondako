b660479c4048aa9683691362358eb73e2c1c6d42
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-02-28 01:52:31 +0900

    🚧 一旦ね

---
+++ src/db/migrations/0005_outgoing_boom_boom.sql
@@ -0,0 +1,136 @@
+CREATE TABLE `verification` (
+  `id` text PRIMARY KEY NOT NULL,
+  `identifier` text NOT NULL,
+  `value` text NOT NULL,
+  `expiresAt` integer NOT NULL,
+  `createdAt` integer,
+  `updatedAt` integer
+);
+--> statement-breakpoint
+DROP TABLE `verificationToken`;--> statement-breakpoint
+
+PRAGMA foreign_keys = OFF;
+
+-- account テーブルの再作成
+CREATE TABLE __new_account (
+  id TEXT PRIMARY KEY NOT NULL,
+  accountId TEXT DEFAULT '' NOT NULL,
+  providerId TEXT NOT NULL,
+  userId TEXT NOT NULL,
+  accessToken TEXT,
+  refreshToken TEXT,
+  idToken TEXT,
+  accessTokenExpiresAt INTEGER,
+  refreshTokenExpiresAt INTEGER,
+  scope TEXT,
+  password TEXT,
+  createdAt INTEGER DEFAULT (CAST(strftime('%s', 'now') AS INTEGER)) NOT NULL,
+  updatedAt INTEGER DEFAULT (CAST(strftime('%s', 'now') AS INTEGER)) NOT NULL,
+  FOREIGN KEY (userId) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+-- idは無いので生成する
+INSERT INTO
+  __new_account (
+    id,
+    accountId,
+    providerId,
+    userId,
+    accessToken,
+    refreshToken,
+    idToken,
+    accessTokenExpiresAt,
+    scope,
+    createdAt,
+    updatedAt
+  )
+SELECT
+  lower(
+    hex(randomblob(4)) || '-' || hex(randomblob(2)) || '-4' || substr(hex(randomblob(2)), 2) || '-' || substr('89ab', abs(random()) % 4 + 1, 1) || substr(hex(randomblob(2)), 2) || '-' || hex(randomblob(6))
+  ),
+  providerAccountId,
+  provider,
+  userId,
+  access_token,
+  refresh_token,
+  id_token,
+  expires_at,
+  scope,
+  (CAST(strftime('%s', 'now') AS INTEGER)),
+  (CAST(strftime('%s', 'now') AS INTEGER))
+FROM
+  account;
+
+DROP TABLE account;
+
+ALTER TABLE __new_account
+RENAME TO account;
+
+-- session テーブルの再作成
+CREATE TABLE __new_session (
+  id TEXT PRIMARY KEY NOT NULL,
+  expiresAt INTEGER NOT NULL,
+  token TEXT,
+  createdAt INTEGER DEFAULT (CAST(strftime('%s', 'now') AS INTEGER)) NOT NULL,
+  updatedAt INTEGER DEFAULT (CAST(strftime('%s', 'now') AS INTEGER)) NOT NULL,
+  ipAddress TEXT,
+  userAgent TEXT,
+  userId TEXT NOT NULL,
+  FOREIGN KEY (userId) REFERENCES user (id) ON UPDATE NO ACTION ON DELETE CASCADE
+);
+
+-- session テーブルのデータ移行（id は新たに生成）
+INSERT INTO
+  __new_session (
+    id,
+    expiresAt,
+    token,
+    createdAt,
+    updatedAt,
+    userId
+  )
+SELECT
+  lower(
+    hex(randomblob(4)) || '-' || hex(randomblob(2)) || '-4' || substr(hex(randomblob(2)), 2) || '-' || substr('89ab', abs(random()) % 4 + 1, 1) || substr(hex(randomblob(2)), 2) || '-' || hex(randomblob(6))
+  ),
+  expires,
+  sessionToken,
+  (CAST(strftime('%s', 'now') AS INTEGER)),
+  (CAST(strftime('%s', 'now') AS INTEGER)),
+  userId
+FROM
+  session;
+
+DROP TABLE session;
+
+ALTER TABLE __new_session
+RENAME TO session;
+
+-- user テーブルの再作成（created_at, updated_at を追加）
+CREATE TABLE __new_user (
+  id TEXT PRIMARY KEY NOT NULL,
+  name TEXT,
+  email TEXT NOT NULL,
+  emailVerified INTEGER,
+  image TEXT,
+  created_at INTEGER DEFAULT (CAST(strftime('%s', 'now') AS INTEGER)) NOT NULL,
+  updated_at INTEGER DEFAULT (CAST(strftime('%s', 'now') AS INTEGER)) NOT NULL
+);
+
+INSERT INTO
+  __new_user (id, name, email, emailVerified, image)
+SELECT
+  id,
+  name,
+  email,
+  emailVerified,
+  image
+FROM
+  user;
+
+DROP TABLE user;
+
+ALTER TABLE __new_user
+RENAME TO user;
+
+PRAGMA foreign_keys = ON;
