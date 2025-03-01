1ea86b1a468cbfc03dd463597cdf7cb6f8ae5a88
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-03-01 21:23:18 +0900

    🩹 Google認証でD1_TYPE_ERRORが発生する

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,29 @@
 import { betterAuth } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { getDB } from "../db";
+import { Kysely } from "kysely";
+import { D1Dialect } from "kysely-d1";
 
 export const getAuth = (dbInstance: D1Database) => {
+  const dbConnection = new D1Dialect({ database: dbInstance });
+  const db = new Kysely({
+    dialect: dbConnection,
+  });
+
   return betterAuth({
     baseURL: process.env.BETTER_AUTH_URL,
-    database: drizzleAdapter(getDB(dbInstance), {
-      provider: "sqlite",
-    }),
+    // NOTE:
+    // 本当は以下のように drizzleAdapter を使いたい
+    // database: drizzleAdapter(getDB(dbInstance), { provider: "sqlite" })
+    //
+    // けど、D1だとこんなエラーが出る
+    // Error: D1_TYPE_ERROR: Type 'object' not supported for value '<日付>']
+    //
+    // 調査したところ、drizzleのアダプタがD1に対応できていないっぽかったのでKyseleyを使うようにしてる
+    // 問題の内容的には、多分これが近いと思う
+    // @see https://github.com/nextauthjs/next-auth/issues/8453
+    database: {
+      db,
+      type: "sqlite",
+    },
     socialProviders: {
       github: {
         clientId: process.env.AUTH_GITHUB_ID || "",
