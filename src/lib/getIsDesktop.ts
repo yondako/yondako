0eae3d104aceee696eb99b2a447fbe8048c0eb01
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-06-04 23:05:26 +0900

    Update src/lib/getIsDesktop.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/lib/getIsDesktop.ts
+++ src/lib/getIsDesktop.ts
@@ -1,7 +1,7 @@
 /**
- * スマホかどうか
+ * デスクトップかどうか
  * @param headers リクエストヘッダー
- * @return スマホの場合はtrue、デスクトップの場合はfalse
+ * @return デスクトップの場合はtrue、スマホの場合はfalse
  */
 export function getIsDesktop(headers: Headers) {
   return headers.get("X-IS-DESKTOP") === "true";
